2d9186a4cd5a0d7f280c4178a915eab587fa3d33
Juan David Salazar Santa <juandsalazars@gmail.com>
2022-11-17 16:30:56 -0500

    Components Part 2 - Props, card and onClick event

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import { DISHES } from './shared/dishes'
 
 function App() {
-    return (
-      <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu/>
-      </div>
-    );
+
+  const dishesList = DISHES;
+
+  return (
+    <div>
+      <Navbar dark color="primary">
+        <div className="container">
+          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+        </div>
+      </Navbar>
+      <Menu dishes={dishesList}/>
+    </div>
+  );
 }
 
 export default App;
